bfb8ebe085a794d45a13e65c28a3669a287e6290
Moises Velasquez <moises007@gmail.com>
2026-09-08 17:28:22 -0500

    feat(logging): centralize application logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { ConfigModule } from '@nestjs/config';
 import { appConfig, configValidationSchema } from './config/index.js';
+import { LoggerModule } from './common/logger/logger.module.js';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { appConfig, configValidationSchema } from './config/index.js';
       load: [appConfig],
       validationSchema: configValidationSchema,
     }),
+    LoggerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ConfigService } from '@nestjs/config';
+import { LoggerService } from './common/index.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  const logger = app.get(LoggerService);
   const port = configService.getOrThrow<number>('app.port');
   await app.listen(port);
-  console.log(`Application is running on: ${await app.getUrl()} 🚀🚀🚀`);
+  logger.log(
+    `Application is running on: ${await app.getUrl()} 👍`,
+    'Bootstrap',
+  );
 }
 await bootstrap();
